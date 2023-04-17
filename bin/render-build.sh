632333800290e70f5d9a5897a04a14474f03ae51
Ubuntu <sakisaki200012@gmail.com>
2023-04-17 05:45:38 +0000

    add data

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,9 +5,10 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
+
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed
+
+
 # productionにする
-
-# bundle exec rails db:migrate:reset
-# bundle exec rails db:seed
-
-bundle exec rails db:migrate
+#bundle exec rails db:migrate
