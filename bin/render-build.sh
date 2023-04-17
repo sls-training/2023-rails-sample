d0a64b6a0575d31a1c3f2bebd054ef21538cf579
Ubuntu <sakisaki200012@gmail.com>
2023-04-17 06:40:27 +0000

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,8 +6,7 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
 
-bundle exec rails db:migrate:reset
-bundle exec rails db:seed
+bundle exec rails db:reset
 
 
 # productionにする
