7f8c4341d6351b04339fdde834f5d90bf8c22ae9
Taku3939 <sakisaki200012@gmail.com>
2023-04-27 05:29:22 +0000

    token発行用apiを作成した

---
+++ app/controllers/api/v1/token_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class TokenController < ActionController::API
+      # POST /api/v1/token
+      def create
+        email =  token_params[:email]
+        password = token_params[:password]
+        user = User.find_by(email: email)
+        if User.inspect(password, user.password_digest)
+          token = encode(user.name)
+
+          render json: token
+        else
+          render json: 'failed'
+        end
+      end
+
+      def update_token(user)
+        user.update_attribute(:authenticate_token, token)
+      end
+
+      def encode(name)
+        payload = { data: name, exp: (Time.zone.now + 3600).to_i }
+        JWT.encode payload, Rails.application.credentials.secret_key_base, 'HS256'
+      end
+
+      def decode(token)
+        JWT.decode token, Rails.application.credentials.secret_key_base, true, { algorithm: 'HS256' }
+      end
+
+      private
+
+      def token_params
+        params.require(:token).permit(:email, :password)
+      end
+    end
+  end
+end
