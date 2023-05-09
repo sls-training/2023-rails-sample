f3e2e53d9894ee173663e8d9f70a2f7e968776a5
Taku3939 <sakisaki200012@gmail.com>
2023-05-09 06:58:46 +0000

    デコードのテストを修正

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -5,13 +5,27 @@ require 'rails_helper'
 RSpec.describe 'AccessToken' do
   let(:email) { 'example@test.com' }
   let(:access_token) { AccessToken.new(email:).encode }
+  let(:invalid_access_token) do
+    issued_at = 11.hours.ago
+    expired_at = issued_at + 1.hour
+    payload = { sub: email, iat: issued_at.to_i, exp: expired_at.to_i }
+    JWT.encode payload, Rails.application.credentials.app.secret_access_key, 'HS256'
+  end
 
   describe '#from_token' do
-    subject { AccessToken.from_token(access_token) }
-
     context 'トークンが正しい場合' do
-      it 'EmailがPayloadに入っている' do
+      subject { AccessToken.from_token(access_token) }
+
+      it 'デコードしてemailが読みとれる' do
         expect(subject.email).to eq email
+      end
+    end
+
+    context 'トークンが期限切れの場合' do
+      subject { AccessToken.from_token(invalid_access_token) }
+
+      it 'デコードに失敗する' do
+        expect(subject).to raise_error(JWT::ExpiredSignature, 'Signature has expired')
       end
     end
   end
