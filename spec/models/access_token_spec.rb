c94ac7d5f48759bf5cc397f6eb9e293d9109934e
Taku3939 <sakisaki200012@gmail.com>
2023-05-09 06:44:11 +0000

    書き方等の変更

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -3,15 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe 'AccessToken' do
-  describe '#encode' do
-  end
+  let(:email) { 'example@test.com' }
+  let(:access_token) { AccessToken.new(email:).encode }
 
   describe '#from_token' do
-    it '生成したアクセストークンが正しくデコードできること' do
-      email = 'uouo@uouo.com'
-      access_token = AccessToken.new(email:).encode
-      token = AccessToken.from_token(access_token)
-      expect(token.email).to eq email
+    subject { AccessToken.from_token(access_token) }
+
+    context 'トークンが正しい場合' do
+      it 'EmailがPayloadに入っている' do
+        expect(subject.email).to eq email
+      end
     end
   end
 end
