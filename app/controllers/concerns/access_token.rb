a26cedf6c5d4c03c9632ceeb2c368e37d3546346
Taku3939 <sakisaki200012@gmail.com>
2023-05-09 01:56:50 +0000

    token_utilをaccess_tokenクラスに変更

---
+++ app/controllers/concerns/access_token.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class AccessToken
+  private_class_method :new
+  attr_reader :email, :exp
+
+  def self.create(email)
+    new email, (Time.zone.now + 3600).to_i
+  end
+
+  def self.from_token(token)
+    payload, = JWT.decode token, Rails.application.credentials.app.secret_access_key, true,
+                          { algorithm: 'HS256' }
+
+    new(payload['email'], payload['exp'])
+  end
+
+  def initialize(email, exp)
+    @email = email
+    @exp = exp
+  end
+
+  def encode
+    payload = { email:, exp: }
+    JWT.encode payload, Rails.application.credentials.app.secret_access_key, 'HS256'
+  end
+end
