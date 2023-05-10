1b1c0b82cc953716f4ca1708230bf0685a5bf11a
Taku3939 <sakisaki200012@gmail.com>
2023-05-10 08:15:22 +0000

    名称の変更

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -8,11 +8,11 @@ module Api
     protected
 
     def authenticate
-      token = request.headers['Authorization']
-      return render json: { message: 'Missing authentication token' }, status: :unauthorized if token.nil?
+      access_token = request.headers['Authorization']
+      return render json: { message: 'Missing authentication token' }, status: :unauthorized if access_token.nil?
 
       begin
-        AccessToken.from_token(token[7..])
+        AccessToken.from_token(access_token[7..])
       rescue JWT::DecodeError, JWT::ExpiredSignature, JWT::VerificationError
         render json: { message: 'Unauthorized. Invalid token' }, status: :unauthorized
       end
