2af58945063ca08dc059751f48fc5c6109a49e6c
Taku3939 <sakisaki200012@gmail.com>
2023-05-16 05:42:30 +0000

    ユーザ作成用のAPIを作成した

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,7 +6,23 @@ module Api
     before_action :validate_user_id, only: %i[show]
     # GET /api/users/:id
     def show
-      render_user user
+      render_user user, :ok
+    end
+
+    # POST /api/users
+    def create
+      name = params[:name]
+      email = params[:email]
+      password = params[:password]
+
+      create_user = User.create(
+        name:,
+        email:,
+        password:,
+        password_confirmation: password,
+        activated:             false
+      )
+      create_user.invalid? ? render_create_failed(create_user) : render_user(create_user, :created)
     end
 
     private
@@ -15,17 +31,24 @@ module Api
       @_user ||= User.find_by(id: params[:id])
     end
 
-    def render_user(user)
-      render status: :ok, json: {
-        id:           user.id,
-        name:         user.name,
-        email:        user.email,
-        admin:        user.admin,
-        activated:    user.activated,
-        activated_at: user.activated_at.iso8601(2),
-        created_at:   user.created_at.iso8601(2),
-        updated_at:   user.updated_at.iso8601(2)
+    def render_user(target, status)
+      render status:, json: {
+        id:           target.id,
+        name:         target.name,
+        email:        target.email,
+        admin:        target.admin,
+        activated:    target.activated,
+        activated_at: target.activated_at&.iso8601(2),
+        created_at:   target.created_at.iso8601(2),
+        updated_at:   target.updated_at.iso8601(2)
       }
+    end
+
+    def render_create_failed(create_user)
+      errors = create_user.errors.map { |err| { name: err.attribute, message: err.message } }
+      is_exist = errors.length == 1 && errors.first[:message] == 'has already been taken'
+      status = is_exist ? :unprocessable_entity : :bad_request
+      render status:, json: { errors: }
     end
 
     def validate_user_id
