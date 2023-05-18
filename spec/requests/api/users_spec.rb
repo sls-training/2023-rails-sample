b9937661c9b345a1a6a065ba8dcd0dd233a5e813
Taku3939 <sakisaki200012@gmail.com>
2023-05-18 05:00:58 +0000

    テストを修正

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -38,29 +38,21 @@ RSpec.describe 'ApiUsers' do
 
       it '401でエラーメッセージを出力して失敗する' do
         expect(subject).to be_unauthorized
-        expect(subject.parsed_body).to have_key('message')
+        expect(subject.parsed_body).to have_key('errors')
       end
     end
 
     context 'アクセストークンがない場合' do
       it '400でエラーメッセージを出力して失敗する' do
         expect(subject).to be_bad_request
-        expect(subject.parsed_body).to have_key('message')
+        expect(subject.parsed_body).to have_key('errors')
       end
     end
   end
 
   describe 'POST /api/users' do
-    subject do
-      post '/api/users', headers:, params: { name:, email:, password: }
-      response
-    end
+    subject { post '/api/users', headers:, params: }
 
-    let(:name) { 'hogehgoe' }
-    let(:email) { 'hoge@example.com' }
-    let(:password) { 'foobar' }
-
-    # rubocop:disable RSpec/MultipleMemoizedHelpers
     context 'アクセストークンが有効の場合' do
       let!(:user) { create(:user, :admin) }
       let(:access_token) { AccessToken.new(email: user.email).encode }
@@ -68,112 +60,80 @@ RSpec.describe 'ApiUsers' do
 
       context 'パラメータが適切な場合' do
         context 'ユーザが存在する場合' do
-          let(:email) { user.email }
+          let(:params) { { name: 'hogehgoe', email: user.email, password: 'foobar' } }
 
           it '422が返って、エラーメッセージを返すこと' do
-            expect(subject).to have_http_status(:unprocessable_entity)
-            expect(subject).to have_error 'email', 'has already been taken'
             expect { subject }.not_to change(User, :count)
+            expect(response).to have_http_status(:unprocessable_entity)
+            expect(response.parsed_body).to have_key('errors')
           end
         end
 
         context 'ユーザが存在しない場合' do
+          let(:params) { { name: 'hogehgoe', email: 'test@example.com', password: 'foobar' } }
+
           it '201が返って、作成したユーザを返すこと' do
-            expect(subject).to be_created
-            expect(subject.parsed_body.symbolize_keys).to include(
-              :id,
-              :name,
-              :admin,
-              :activated,
-              :activated_at,
-              :created_at,
-              :updated_at
-            )
             expect { subject }.to change(User, :count).by(1)
+            expect(response).to be_created
+            expect(response.parsed_body).to include(
+              *%w[
+                id
+                name
+                admin
+                activated
+                activated_at
+                created_at
+                updated_at
+              ]
+            )
           end
         end
       end
 
-      context '名前がない場合' do
-        let(:name) { '' }
-
-        it '400が返って、エラーメッセージを返すこと' do
-          expect(subject).to be_bad_request
-          expect(subject).to have_error 'name', "can't be blank"
+      context 'パラメータが適切でない場合' do
+        let(:wrong_cases) do
+          [
+            { name: '', email: 'test@example.com', password: 'foobar' },
+            { name: 'hogehgoe', email: '', password: 'foobar' },
+            { name: 'hogehoge', email: 'test@example.com', password: '' },
+            * %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com].map do |addr|
+              { name: 'hogehgoe', email: addr, password: 'foobar' }
+            end,
+            { name: 'a' * 51, email: 'test@example.com', password: 'foobar'  },
+            { name: 'hogehgoe', email: "#{'a' * 244}@example.com", password: 'foobar' },
+            { name: 'hogehoge', email: 'test@example.com', password: 'a' * 5 }
+          ]
         end
-      end
-
-      context 'メールがない場合' do
-        let(:email) { '' }
 
         it '400が返って、エラーメッセージを返すこと' do
-          expect(subject).to be_bad_request
-          expect(subject).to have_error 'email', 'is invalid'
-        end
-      end
-
-      context 'パスワードがない場合' do
-        let(:password) { '' }
-
-        it '400が返って、エラーメッセージを返すこと' do
-          expect(subject).to be_bad_request
-          expect(subject).to have_error 'password', "can't be blank"
-        end
-      end
-
-      context 'メールの形式が間違っている場合' do
-        let(:email) { 'invalid@email' }
-
-        it '400が返って、エラーメッセージを返すこと' do
-          invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
-          invalid_addresses.each do |_invalid_address|
-            expect(subject).to be_bad_request
-            expect(subject).to have_error 'email', 'is invalid'
+          wrong_cases.each do |wrong_case|
+            expect { post '/api/users', headers:, params: wrong_case }.not_to change(User, :count)
+            expect(response).to be_bad_request
+            expect(response.parsed_body).to have_key('errors')
           end
-        end
-      end
-
-      context '名前が長すぎる場合' do
-        let(:name) { 'a' * 51 }
-
-        it '400が返って、エラーメッセージを返すこと' do
-          expect(subject).to be_bad_request
-          expect(subject).to have_error 'name', 'is too long (maximum is 50 characters)'
-        end
-      end
-
-      context 'メールが長すぎる場合' do
-        let(:email) { "#{'a' * 244}@example.com" }
-
-        it '400が返って、エラーメッセージを返すこと' do
-          expect(subject).to be_bad_request
-          expect(subject).to have_error 'email', 'is too long (maximum is 255 characters)'
-        end
-      end
-
-      context 'パスワードが短すぎる場合' do
-        let(:password) { 'a' * 5 }
-
-        it '400が返って、エラーメッセージを返すこと' do
-          expect(subject).to be_bad_request
-          expect(subject).to have_error 'password', 'is too short (minimum is 6 characters)'
         end
       end
     end
 
     context 'アクセストークンが有効期限切れの場合' do
+      let(:email) { 'test@example.com' }
       let(:headers) { { 'Authorization' => "Bearer #{expired_access_token(email:)}" } }
+      let(:params) { { name: 'hogehgoe', email: 'test@example.com', password: 'foobar' } }
 
       it '401でエラーメッセージを出力して失敗する' do
-        expect(subject).to be_unauthorized
-        expect(subject.parsed_body).to have_key('message')
+        expect { subject }.not_to change(User, :count)
+        expect(response).to be_unauthorized
+        expect(response.parsed_body).to have_key('errors')
       end
     end
 
     context 'アクセストークンがない場合' do
+      let(:params) { { name: 'hogehgoe', email: 'test@example.com', password: 'foobar' } }
+
       it '400でエラーメッセージを出力して失敗する' do
-        expect(subject).to be_bad_request
-        expect(subject.parsed_body).to have_key('message')
+        expect { subject }.not_to change(User, :count)
+        expect(response).to have_http_status(:bad_request)
+        expect(response.parsed_body).to have_key('errors')
       end
     end
   end
