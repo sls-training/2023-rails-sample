8e4dcfb3dd8996fffe72a9a6afdbd4f8cdfd37e5
Taku3939 <sakisaki200012@gmail.com>
2023-05-19 06:58:45 +0000

    トークンのユーザを保持するように変更

--- app/controllers/concerns/access_token_verifiable.rb
+++ app/controllers/concerns/access_token_verifiable.rb
@@ -7,6 +7,10 @@ module AccessTokenVerifiable
     before_action :verify_access_token_in_header
   end
 
+  def token_user
+    @_token_user
+  end
+
   def verify_access_token_in_header
     authorization_header = request.headers['Authorization']
     access_token = authorization_header&.match(/Bearer (?<token>.+)/)&.[](:token)
@@ -14,7 +18,8 @@ module AccessTokenVerifiable
     return render_missing_token if access_token.nil?
 
     begin
-      AccessToken.from_token(access_token)
+      email = AccessToken.from_token(access_token).email
+      @_token_user = User.find_by(email:)
     rescue JWT::DecodeError
       render_invalid_token
     end
