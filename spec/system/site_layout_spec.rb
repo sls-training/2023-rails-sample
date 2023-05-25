ff4cf93814213442454eb2a489b808af5b044852
Taku3939 <sakisaki200012@gmail.com>
2023-05-25 02:57:04 +0000

    テストが通るように修正

--- spec/system/site_layout_spec.rb
+++ spec/system/site_layout_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe 'SiteLayout' do
 
     before { visit root_path }
 
-    it('has root path') { expect(body).to have_link 'Home', href: root_path }
-    it('has help path') { expect(body).to have_link 'Help', href: help_path }
-    it('has contact path') { expect(body).to have_link 'Contact', href: contact_path }
-    it('has about path') { expect(body).to have_link 'About', href: about_path }
-    it('has singup path') { expect(body).to have_link 'Sign up now!', href: signup_path }
+    it('has root path') { expect(body).to have_link 'ホーム', href: root_path }
+    it('has help path') { expect(body).to have_link 'ヘルプ', href: help_path }
+    it('has contact path') { expect(body).to have_link 'お問い合わせ', href: contact_path }
+    it('has about path') { expect(body).to have_link 'このサイトについて', href: about_path }
+    it('has singup path') { expect(body).to have_link 'サインアップ', href: signup_path }
   end
 end
