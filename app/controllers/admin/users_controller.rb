50fdd23df58d94b5fb1bb9408c5399f0bd533b14
Taku3939 <sakisaki200012@gmail.com>
2023-05-31 03:40:01 +0000

    管理者ユーザでログインしているかをチェック

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'net/https'
+require 'uri'
+
+module Admin
+  class UsersController < ApplicationController
+    before_action :logged_in_admin_user
+    def index; end
+
+    def logged_in_admin_user
+      return if logged_in? && current_user.admin?
+
+      store_location
+      flash[:danger] = 'エラー : 管理者ユーザでログインしてください'
+      redirect_to login_url, status: :see_other
+    end
+  end
+end
