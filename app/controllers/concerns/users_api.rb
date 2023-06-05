9e86455c730de201f0e984c7f4f58de17df1c65f
Taku3939 <sakisaki200012@gmail.com>
2023-06-05 01:03:06 +0000

    トークン検証と生成を行う部分の作成

--- app/controllers/concerns/users_api.rb
+++ app/controllers/concerns/users_api.rb
@@ -5,6 +5,14 @@ require 'uri'
 
 module UsersApi
   BASE_URL = 'http://localhost:3000/api'
+  EMAIL =  Rails.application.credentials.app.rails_sample_email
+  PASSWORD = Rails.application.credentials.app.rails_sample_password
+
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :generate_access_token
+  end
 
   private
 
@@ -24,6 +32,28 @@ module UsersApi
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.scheme == 'https'
     http.post(uri.path, params.to_json, headers)
+  end
+
+  def raw_access_token
+    @raw_access_token ||= create_token(EMAIL, PASSWORD)
+  end
+
+  def access_token
+    @_access_token ||= AccessToken.from_token(raw_access_token) if raw_access_token.present?
+  end
+
+  def generate_access_token
+    redirect_to_login if raw_access_token.nil?
+
+    begin
+      access_token
+    rescue JWT::DecodeError
+      redirect_to_login
+    end
+  end
+
+  def redirect_to_login
+    redirect_to login_url, flash: { danger: 'ログインし直してください' }
   end
 
   def create_token(email, password)
