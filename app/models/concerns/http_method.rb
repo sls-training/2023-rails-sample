1cfc042907a45390db86df22892a70be28a14189
Taku3939 <sakisaki200012@gmail.com>
2023-06-06 09:22:37 +0000

    File.joinに変更

--- app/models/concerns/http_method.rb
+++ app/models/concerns/http_method.rb
@@ -7,7 +7,7 @@ module HttpMethod
   BASE_URL = 'http://localhost:3000/api'
 
   def get(url_path, params: {}, headers: {})
-    uri = URI("#{BASE_URL}#{url_path}")
+    uri = URI(File.join(BASE_URL, url_path))
     uri.query = URI.encode_www_form(params)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.scheme == 'https'
@@ -15,7 +15,7 @@ module HttpMethod
   end
 
   def post(url_path, params: {}, headers: {})
-    uri = URI("#{BASE_URL}#{url_path}")
+    uri = URI(File.join(BASE_URL, url_path))
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.scheme == 'https'
     http.post(uri.path, params.to_json, headers)
