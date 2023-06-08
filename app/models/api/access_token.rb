102f8b05b139498499be18ae40a50f504f5c2157
Taku3939 <sakisaki200012@gmail.com>
2023-06-08 06:52:18 +0000

    createに変更

--- app/models/api/access_token.rb
+++ app/models/api/access_token.rb
@@ -6,17 +6,16 @@ module Api
     attr_reader :value
     attr_reader :errors
 
-    def initialize(email:, password:)
+    def create(email:, password:)
       response = post(
         '/token', params:  { email:, password: },
                   headers: { 'Content-Type' => 'application/json' }
       )
+      data = JSON.parse(response.body, symbolize_names: true)
       case response
       when Net::HTTPSuccess
-        data = JSON.parse(response.body, symbolize_names: true)
         @value = data[:access_token]
       else
-        data = JSON.parse(response.body, symbolize_names: true)
         @errors = data[:errors]
       end
     end
