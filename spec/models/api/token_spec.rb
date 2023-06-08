c6bf0c979d9f3eb1c089add12b4b8fb70044c3b3
Taku3939 <sakisaki200012@gmail.com>
2023-06-08 02:49:08 +0000

    順番を変更

--- spec/models/api/token_spec.rb
+++ spec/models/api/token_spec.rb
@@ -6,16 +6,8 @@ RSpec.describe 'Token' do
   describe '#create' do
     subject { Api::Token.create(email:, password:) }
 
-    context '管理者ユーザの場合' do
-      let(:email) { Rails.application.credentials.app.rails_sample_email }
-
-      context 'emailとパスワードが正しい場合' do
-        let(:password) { Rails.application.credentials.app.rails_sample_password }
-
-        it 'アクセストークンが返る' do
-          expect(subject).to have_key(:access_token)
-        end
-      end
+    context '管理者でないユーザの場合' do
+      let(:email) { 'example-1@railstutorial.org' }
 
       context 'パスワードが間違っている場合' do
         let(:password) { 'wrong_password' }
@@ -24,10 +16,6 @@ RSpec.describe 'Token' do
           expect(subject).to have_key(:errors)
         end
       end
-    end
-
-    context '管理者でないユーザの場合' do
-      let(:email) { 'example-1@railstutorial.org' }
 
       context 'emailとパスワードが正しい場合' do
         let(:password) { 'password' }
@@ -36,12 +24,24 @@ RSpec.describe 'Token' do
           expect(subject).to have_key(:errors)
         end
       end
+    end
+
+    context '管理者ユーザの場合' do
+      let(:email) { Rails.application.credentials.app.rails_sample_email }
 
       context 'パスワードが間違っている場合' do
         let(:password) { 'wrong_password' }
 
         it 'errorsが返る' do
           expect(subject).to have_key(:errors)
+        end
+      end
+
+      context 'emailとパスワードが正しい場合' do
+        let(:password) { Rails.application.credentials.app.rails_sample_password }
+
+        it 'アクセストークンが返る' do
+          expect(subject).to have_key(:access_token)
         end
       end
     end
