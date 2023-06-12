fe2f02f9b27a25d9e95f14a86cbfb373e2a3197d
Taku3939 <sakisaki200012@gmail.com>
2023-06-12 06:28:26 +0000

    Csrfトークンを取得する関数を追加

--- app/javascript/components/modal.tsx
+++ app/javascript/components/modal.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import User from '../types/user';
 
+export const getCsrfToken = (): string => {
+  const metaElement = document.querySelector("meta[name='csrf-token']");
+  if (!metaElement) throw Error('meta[name=csrf-token] is not founded.');
+
+  const csrfToken = metaElement.getAttribute('content');
+  if (!csrfToken) throw Error('csrf token is not set');
+
+  return csrfToken;
+};
+
 export const CreationModal = () => {
   return <div className="modal-content">{/* TDDO:ここにユーザ作成用のモーダルの中身を作成する */}</div>;
 };
