265ac27580b51eb35ed70a00fbebe66d0c1e8bf5
Taku3939 <sakisaki200012@gmail.com>
2023-06-13 01:18:53 +0000

    errorにfrom_jsonの追加

--- app/models/api/error.rb
+++ app/models/api/error.rb
@@ -4,6 +4,10 @@ module Api
   class Error
     attr_reader :name, :message
 
+    def self.from_json(json)
+      Api::Error.new(name: json[:name], message: json[:message])
+    end
+
     def initialize(name:, message:)
       @name = name
       @message = message
