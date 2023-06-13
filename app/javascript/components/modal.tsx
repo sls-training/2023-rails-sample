79ef84ae1a7341a1a3e89eb016aabc4dd19417a6
Taku3939 <sakisaki200012@gmail.com>
2023-06-13 02:38:05 +0000

    ユーザ作成のモーダルを作成

--- app/javascript/components/modal.tsx
+++ app/javascript/components/modal.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import User from '../types/user';
 
-export const CreationModal = () => {
-  return <div className="modal-content">{/* TDDO:ここにユーザ作成用のモーダルの中身を作成する */}</div>;
-};
+export const CreationModal = () => (
+  <div className="modal-content">
+    <div className="modal-header">
+      <h5 className="modal-title" id="createUserModalTitle">
+        ユーザを作成
+      </h5>
+      <button type="button" className="btn btn-secondary close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <form method="post" action={`/admin/users`}>
+      <div className="modal-body">
+        <label htmlFor="name">名前</label>
+        <input className="form-control" type="text" id="name" name="name" required />
+        <label htmlFor="email">メール</label>
+        <input className="form-control" type="text" id="email" name="email" required />
+        <label htmlFor="password">パスワード</label>
+        <input className="form-control" type="text" id="password" name="password" required />
+        <label htmlFor="password_confirmation">パスワードをもう一度入力</label>
+        <input className="form-control" type="text" id="password_confirmation" name="password_confirmation" required />
+      </div>
+      <div className="modal-footer">
+        <input type="button" className="btn btn-secondary" data-dismiss="modal" value="いいえ" />
+
+        <input className="btn btn-primary" type="submit" value="はい" />
+        <input name="authenticity_token" type="hidden" value={getCsrfToken()} />
+        <input name="_method" type="hidden" value="POST" />
+      </div>
+    </form>
+  </div>
+);
 
 export const EditationModal = ({ user }: { user: User }) => {
   return <div className="modal-content">{/* TDDO:ここにユーザ編集用のモーダルの中身を作成する */}</div>;
