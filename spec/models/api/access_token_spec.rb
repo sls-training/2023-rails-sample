db943bccc5aad0dd42f94db9359309233490d2e9
Taku3939 <sakisaki200012@gmail.com>
2023-06-13 01:11:50 +0000

    ミスの修正

--- spec/models/api/access_token_spec.rb
+++ spec/models/api/access_token_spec.rb
@@ -2,12 +2,11 @@
 
 require 'rails_helper'
 
-RSpec.describe 'User' do
-  describe '#get_list' do
-    subject { Api::User.get_list(access_token:) }
+RSpec.describe 'AccessToken' do
+  describe '#create' do
+    subject { Api::AccessToken.create(email: user.email, password: user.password) }
 
     let(:user) { create(:user, :admin) }
-    let(:access_token) { AccessToken.new(email: user.email).encode }
 
     it '配列のオブジェクトをレスポンスとして取得する' do
       expect(subject.instance_of?(Array)).to be true
