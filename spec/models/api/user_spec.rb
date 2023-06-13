499db3d79fcd7e6f9f06999ae0bcb251d353c9cf
Taku3939 <sakisaki200012@gmail.com>
2023-06-13 08:53:18 +0000

    specを戻した

--- spec/models/api/user_spec.rb
+++ spec/models/api/user_spec.rb
@@ -6,11 +6,32 @@ RSpec.describe 'User' do
   describe '#get_list' do
     subject { Api::User.get_list(access_token:) }
 
-    let(:user) { create(:user, :admin) }
-    let(:access_token) { AccessToken.new(email: user.email).encode }
+    context 'アクセストークンがない場合' do
+      let(:access_token) { nil }
 
-    it '配列のオブジェクトをレスポンスとして取得する' do
-      expect(subject.instance_of?(Array)).to be true
+      it 'errorsが返る' do
+        expect(subject).to have_key(:errors)
+      end
+    end
+
+    context 'アクセストークンがある場合' do
+      let(:current_user) { create(:user, :admin) }
+
+      context 'アクセストークンが有効期限切れの場合' do
+        let(:access_token) { expired_access_token(email: current_user.email) }
+
+        it 'errorsが返る' do
+          expect(subject).to have_key(:errors)
+        end
+      end
+
+      context 'アクセストークンが有効期限内の場合' do
+        let(:access_token) { AccessToken.new(email: current_user.email).encode }
+
+        it 'ユーザの配列が返る' do
+          expect(subject.instance_of?(Array)).to be true
+        end
+      end
     end
   end
 end
