5dcdad8d6501ad44efcd8345c6bee1ac8854bd06
Taku3939 <sakisaki200012@gmail.com>
2023-06-15 08:48:25 +0000

    ログインしたときにToken生成用のAPIが呼べるように変更

--- spec/support/sessions_support.rb
+++ spec/support/sessions_support.rb
@@ -15,6 +15,10 @@ module SessionsSupport
 
   # テストユーザとしてログインする
   def log_in_as(user, password: 'password', remember_me: '1')
+    WebMock.stub_request(:post, 'http://localhost:3000/api/token')
+      .with(body: { email: user.email, password: })
+      .to_return(body: { access_token: AccessToken.new(email: user.email).encode }.to_json, status: 200, headers: { 'Content-Type' => 'application/json' })
+
     post login_path, params: { session: { email: user.email, password:, remember_me: } }
   end
 
