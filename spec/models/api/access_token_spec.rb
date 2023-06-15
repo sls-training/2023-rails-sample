d07683db674e397d03df4664851f16cedab0e4dc
Taku3939 <sakisaki200012@gmail.com>
2023-06-15 05:48:00 +0000

    アクセストークンのテストをwebmockを使って書き換え

--- spec/models/api/access_token_spec.rb
+++ spec/models/api/access_token_spec.rb
@@ -7,11 +7,30 @@ RSpec.describe 'AccessToken' do
   describe '#create' do
     subject { Api::AccessToken.create(email:, password:) }
 
+    before { WebMock.enable! }
+
     context '管理者でないユーザの場合' do
       let(:email) { '' }
       let(:password) { 'password' }
 
       context 'emailがない場合' do
+        before do
+          WebMock
+            .stub_request(:post, 'http://localhost:3000/api/token')
+            .with(body: { email:, password: })
+            .to_return(
+              body:   {
+                errors: [
+                  {
+                    name:    'access_token',
+                    message: 'Incorrect email or password'
+                  }
+                ]
+              }.to_json,
+              status: 400
+            )
+        end
+
         it 'errorが返る' do
           expect(subject).to include Api::Error
         end
@@ -23,6 +42,23 @@ RSpec.describe 'AccessToken' do
         context 'パスワードが間違っている場合' do
           let(:password) { 'wrong_password' }
 
+          before do
+            WebMock
+              .stub_request(:post, 'http://localhost:3000/api/token')
+              .with(body: { email:, password: })
+              .to_return(
+                body:   {
+                  errors: [
+                    {
+                      name:    'access_token',
+                      message: 'Incorrect email or password'
+                    }
+                  ]
+                }.to_json,
+                status: 400
+              )
+          end
+
           it 'errorが返る' do
             expect(subject).to include Api::Error
           end
@@ -30,6 +66,23 @@ RSpec.describe 'AccessToken' do
 
         context 'emailとパスワードが正しい場合' do
           let(:password) { 'password' }
+
+          before do
+            WebMock
+              .stub_request(:post, 'http://localhost:3000/api/token')
+              .with(body: { email:, password: })
+              .to_return(
+                body:   {
+                  errors: [
+                    {
+                      name:    'access_token',
+                      message: 'You are not admin user'
+                    }
+                  ]
+                }.to_json,
+                status: 403
+              )
+          end
 
           it 'errorが返る' do
             expect(subject).to include Api::Error
@@ -43,6 +96,23 @@ RSpec.describe 'AccessToken' do
         let(:email) { '' }
         let(:password) { 'wrong_password' }
 
+        before do
+          WebMock
+            .stub_request(:post, 'http://localhost:3000/api/token')
+            .with(body: { email:, password: })
+            .to_return(
+              body:   {
+                errors: [
+                  {
+                    name:    'access_token',
+                    message: 'Incorrect email or password'
+                  }
+                ]
+              }.to_json,
+              status: 400
+            )
+        end
+
         it 'errorが返る' do
           expect(subject).to include Api::Error
         end
@@ -54,6 +124,23 @@ RSpec.describe 'AccessToken' do
         context 'パスワードが間違っている場合' do
           let(:password) { 'wrong_password' }
 
+          before do
+            WebMock
+              .stub_request(:post, 'http://localhost:3000/api/token')
+              .with(body: { email:, password: })
+              .to_return(
+                body:   {
+                  errors: [
+                    {
+                      name:    'access_token',
+                      message: 'Incorrect email or password'
+                    }
+                  ]
+                }.to_json,
+                status: 400
+              )
+          end
+
           it 'errorが返る' do
             expect(subject).to include Api::Error
           end
@@ -61,6 +148,18 @@ RSpec.describe 'AccessToken' do
 
         context 'emailとパスワードが正しい場合' do
           let(:password) { Rails.application.credentials.app.rails_sample_password }
+
+          before do
+            WebMock
+              .stub_request(:post, 'http://localhost:3000/api/token')
+              .with(body: { email:, password: })
+              .to_return(
+                body:   {
+                  access_token: AccessToken.new(email:).encode
+                }.to_json,
+                status: 200
+              )
+          end
 
           it 'アクセストークンが返る' do
             expect(subject.value).not_to be_nil
