83e1fb171c67acf92b76fa4825181b6e1b98b21b
Taku3939 <sakisaki200012@gmail.com>
2023-06-19 07:01:15 +0000

    userを変換する関数をコンパクトにした

--- spec/models/api/user_spec.rb
+++ spec/models/api/user_spec.rb
@@ -70,19 +70,17 @@ RSpec.describe 'User' do
 
       context 'アクセストークンが有効期限内の場合' do
         before do
-          def users_to_json(users)
-            users.map do |user|
-              {
-                id:           user.id,
-                name:         user.name,
-                email:        user.email,
-                admin:        user.admin,
-                activated:    user.activated,
-                activated_at: user.activated_at&.iso8601(2),
-                created_at:   user.created_at.iso8601(2),
-                updated_at:   user.updated_at.iso8601(2)
-              }
-            end.to_json
+          def user_to_api_user(user)
+            {
+              id:           user.id,
+              name:         user.name,
+              email:        user.email,
+              admin:        user.admin,
+              activated:    user.activated,
+              activated_at: user.activated_at&.iso8601(2),
+              created_at:   user.created_at.iso8601(2),
+              updated_at:   user.updated_at.iso8601(2)
+            }
           end
           WebMock
             .stub_request(:get, 'http://localhost:3000/api/users')
@@ -91,12 +89,11 @@ RSpec.describe 'User' do
               headers: { Authorization: "Bearer #{access_token}" }
             )
             .to_return(
-              body:    users_to_json(
-                User
-                  .order(sort_key => order_by)
-                  .limit(limit)
-                  .offset(offset)
-              ),
+              body:    User
+                       .order(sort_key => order_by)
+                       .limit(limit)
+                       .offset(offset).map { |user| user_to_api_user(user) }
+                       .to_json,
               status:  200,
               headers: { 'Content-Type' => 'application/json' }
             )
