61307ed13b81c2b8dcdbbfaf04108a6aa26e5ed0
Taku3939 <sakisaki200012@gmail.com>
2023-06-19 02:34:17 +0000

    specを作成

--- spec/models/api/user_spec.rb
+++ spec/models/api/user_spec.rb
@@ -34,4 +34,41 @@ RSpec.describe 'User' do
       end
     end
   end
+
+  describe '#create' do
+    subject { Api::User.create(access_token:, name:, email:, password:, admin:) }
+
+    let(:name) { Faker::Name.name }
+    let(:email) { Faker::Internet.email }
+    let(:password) { Faker::Internet.password(min_length: 6) }
+    let(:admin) { [true, false].sample }
+
+    context 'アクセストークンがない場合' do
+      let(:access_token) { nil }
+
+      it '例外が返る' do
+        expect { subject }.to raise_error Api::Error
+      end
+    end
+
+    context 'アクセストークンがある場合' do
+      let(:current_user) { create(:user, :admin) }
+
+      context 'アクセストークンが有効期限切れの場合' do
+        let(:access_token) { expired_access_token(email: current_user.email) }
+
+        it '例外が返る' do
+          expect { subject }.to raise_error Api::Error
+        end
+      end
+
+      context 'アクセストークンが有効期限内の場合' do
+        let(:access_token) { AccessToken.new(email: current_user.email).encode }
+
+        it 'ユーザが返る' do
+          expect(subject).to be_instance_of Api::User
+        end
+      end
+    end
+  end
 end
