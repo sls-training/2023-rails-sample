d1c569b8156ecb0d3ee02b50bf42bf581b2a8ae6
Taku3939 <sakisaki200012@gmail.com>
2023-06-20 05:44:37 +0000

    ユーザ取得部分の仕様を作成した

--- spec/requests/admin/users_spec.rb
+++ spec/requests/admin/users_spec.rb
@@ -2,25 +2,45 @@
 
 require 'rails_helper'
 RSpec.describe 'AdminUsers' do
-  let!(:admin_user) { create(:user, :admin) }
-  let!(:non_admin_user) { create(:user, :noadmin) }
-
   describe 'GET /admin/users' do
-    subject { get admin_users_path }
-
     context 'ユーザが管理者の場合' do
-      before { log_in_as(admin_user) }
+      context 'パスワードがあっている場合' do
+        context 'pageが0の場合' do
+          xit 'ユーザ取得時のoffsetが0で200が返る' do
+            # TODO: offsetが0で200が返るspecを作成する
+          end
 
-      it '200が返って、アクセスできる' do
-        subject
-        expect(response).to be_successful
+          xit 'ユーザの表示数は10' do
+            # TODO: ユーザの表示数は10のspecを作成する
+          end
+        end
+
+        context 'pageが1の場合' do
+          xit 'ユーザ取得時のoffsetが0で200が返る' do
+            # TODO: offsetが0で200が返るspecを作成する
+          end
+
+          xit 'ユーザの表示数は10' do
+            # TODO: ユーザの表示数は10のspecを作成する
+          end
+        end
+
+        context 'pageが2の場合' do
+          xit 'ユーザ取得時のoffsetが10で200が返る' do
+            # TODO: offsetが10で200が返るspecを作成する
+          end
+
+          xit 'ユーザの表示数は10' do
+            # TODO: ユーザの表示数は10のspecを作成する
+          end
+        end
       end
     end
 
     context 'ユーザが管理者ではない場合' do
       before { log_in_as(non_admin_user) }
 
-      it 'ログインページにリダイレクトしてトーストメッセージを表示' do
+      it 'ログインページにリダイレクトしてトーストメッセージを表示する' do
         expect(subject).to redirect_to login_url
         expect(response).to have_http_status :see_other
         expect(flash[:danger]).not_to be_nil
@@ -28,7 +48,7 @@ RSpec.describe 'AdminUsers' do
     end
 
     context 'ログインしていない場合' do
-      it 'ログインページにリダイレクトしてトーストメッセージを表示' do
+      it 'ログインページにリダイレクトしてトーストメッセージを表示する' do
         expect(subject).to redirect_to login_url
         expect(response).to have_http_status :see_other
         expect(flash[:danger]).not_to be_nil
