4d46627cfd7b2729fbce8aaca1daa8b4a6ab9b11
Taku3939 <sakisaki200012@gmail.com>
2023-06-21 07:53:13 +0000

    specの中身を作成

--- spec/models/api/user_spec.rb
+++ spec/models/api/user_spec.rb
@@ -109,25 +109,117 @@ RSpec.describe 'User' do
   end
 
   describe '#create' do
+    subject { Api::User.create(access_token:, name:, email:, password:) }
+
+    let(:name) { Faker::Name.name }
+    let(:email) { Faker::Internet.email }
+    let(:password) { Faker::Internet.password(min_length: 6) }
+
     context 'アクセストークンがない場合' do
-      xit 'ユーザ作成のAPIを呼び、例外を返す' do
+      let(:access_token) { nil }
+
+      before do
+        WebMock
+          .stub_request(:post, 'http://localhost:3000/api/users')
+          .with(body: { name:, email:, password: })
+          .to_return(
+            body:    {
+              errors: [
+                {
+                  name:    'access_token',
+                  message: 'Invalid token'
+                }
+              ]
+            }.to_json,
+            status:  400,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+      end
+
+      it 'ユーザ作成のAPIを呼び、例外を返す' do
+        expect { subject }.to raise_error Api::Error
       end
     end
 
     context 'アクセストークンがある場合' do
+      let(:current_user) { create(:user, :admin) }
+
       context 'アクセストークンが有効期限切れの場合' do
-        xit 'ユーザ作成のAPIを呼び、例外を返す' do
+        let(:access_token) { expired_access_token(email: current_user.email) }
+
+        before do
+          WebMock
+            .stub_request(:post, 'http://localhost:3000/api/users')
+            .with(
+              body:    { name:, email:, password: },
+              headers: { Authorization: "Bearer #{access_token}" }
+            )
+            .to_return(
+              body:    {
+                errors: [
+                  {
+                    name:    'access_token',
+                    message: 'Invalid token'
+                  }
+                ]
+              }.to_json,
+              status:  400,
+              headers: { 'Content-Type' => 'application/json' }
+            )
+        end
+
+        it 'ユーザ作成のAPIを呼び、例外を返す' do
+          expect { subject }.to raise_error Api::Error
         end
       end
 
       context 'アクセストークンが有効期限内の場合' do
+        let(:access_token) { AccessToken.new(email: current_user.email).encode }
+
         context '不正なユーザーデータが指定された場合' do
-          xit 'ユーザ作成のAPIを呼び、例外を返す' do
+          before do
+            WebMock
+              .stub_request(:post, 'http://localhost:3000/api/users')
+              .with(
+                body:    { name:, email:, password: },
+                headers: { Authorization: "Bearer #{access_token}" }
+              )
+              .to_return(
+                body:    {
+                  errors: [
+                    {
+                      name:    'access_token',
+                      message: 'Invalid token'
+                    }
+                  ]
+                }.to_json,
+                status:  401,
+                headers: { 'Content-Type' => 'application/json' }
+              )
+          end
+
+          it 'ユーザ作成のAPIを呼び、例外を返す' do
+            expect { subject }.to raise_error Api::Error
           end
         end
 
         context '正当なユーザーデータが指定された場合' do
-          xit 'ユーザ作成のAPIを呼び、作成されたユーザを返す' do
+          before do
+            WebMock
+              .stub_request(:post, 'http://localhost:3000/api/users')
+              .with(
+                body:    { name:, email:, password: },
+                headers: { Authorization: "Bearer #{access_token}" }
+              )
+              .to_return(
+                body:    user_to_api_user(User.last).to_json,
+                status:  200,
+                headers: { 'Content-Type' => 'application/json' }
+              )
+          end
+
+          it 'ユーザ作成のAPIを呼び、作成されたユーザを返す' do
+            expect(subject).to be_instance_of Api::User
           end
         end
       end
