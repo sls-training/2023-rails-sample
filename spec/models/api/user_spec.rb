e9098422e2681d9bbf2158157aefc91ba842e856
Taku3939 <sakisaki200012@gmail.com>
2023-06-22 05:08:55 +0000

    トークンの有無の部分の削除

--- spec/models/api/user_spec.rb
+++ spec/models/api/user_spec.rb
@@ -109,27 +109,13 @@ RSpec.describe 'User' do
   end
 
   describe '#create' do
-    context 'アクセストークンがない場合' do
+    context '不正なユーザーデータ、アクセストークンが指定された場合' do
       xit 'ユーザ作成のAPIを呼び、例外を返す' do
       end
     end
 
-    context 'アクセストークンがある場合' do
-      context 'アクセストークンが有効期限切れの場合' do
-        xit 'ユーザ作成のAPIを呼び、例外を返す' do
-        end
-      end
-
-      context 'アクセストークンが有効期限内の場合' do
-        context '不正なユーザーデータが指定された場合' do
-          xit 'ユーザ作成のAPIを呼び、例外を返す' do
-          end
-        end
-
-        context '正当なユーザーデータが指定された場合' do
-          xit 'ユーザ作成のAPIを呼び、作成されたユーザを返す' do
-          end
-        end
+    context '正当なユーザーデータ、アクセストークンが指定された場合' do
+      xit 'ユーザ作成のAPIを呼び、作成されたユーザを返す' do
       end
     end
   end
