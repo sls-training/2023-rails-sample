c23c1a6034800efe59f575925804421dc5d8d516
Taku3939 <sakisaki200012@gmail.com>
2023-06-26 09:10:05 +0000

    関数の実行時にが不要なので削除

--- spec/requests/admin/users_spec.rb
+++ spec/requests/admin/users_spec.rb
@@ -96,13 +96,13 @@ RSpec.describe 'AdminUsers' do
       end
 
       context 'ユーザが管理者の場合' do
-        context 'ユーザ作成の関数の実行時に例外が返ってきた場合' do
+        context '例外が返ってきた場合' do
           xit 'ユーザ管理画面にリダイレクトして、作成に失敗した旨をトーストメッセージで表示する' do
             # TODO: specの内容を作成する
           end
         end
 
-        context 'ユーザ作成の関数の実行時にユーザが返ってきた場合' do
+        context 'ユーザが返ってきた場合' do
           xit 'ユーザ管理画面にリダイレクトして、作成に成功した旨をトーストメッセージで表示する' do
             # TODO: specの内容を作成する
           end
@@ -126,13 +126,13 @@ RSpec.describe 'AdminUsers' do
       end
 
       context 'ユーザが管理者の場合' do
-        context 'ユーザ更新の関数の実行時に例外が返ってきた場合' do
+        context '例外が返ってきた場合' do
           xit 'ユーザ管理画面にリダイレクトして、編集に失敗した旨をトーストメッセージで表示する' do
             # TODO: specの内容を作成する
           end
         end
 
-        context 'ユーザ更新の関数の実行時にユーザが返ってきた場合' do
+        context 'ユーザが返ってきた場合' do
           xit '編集に成功した旨をトーストメッセージで表示して、200を返す' do
             # TODO: specの内容を作成する
           end
