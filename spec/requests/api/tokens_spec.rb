019254893f780518f8c3af5b7da71194fdf77bf2
Taku3939 <sakisaki200012@gmail.com>
2023-06-28 02:10:27 +0000

    スキーマ通りかは後続の文章からわかるため削除

--- spec/requests/api/tokens_spec.rb
+++ spec/requests/api/tokens_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'ApiTokens' do
         let(:user) { create(:user, :admin) }
         let(:headers) { { 'Content-Type': 'application/json' } }
 
-        it 'スキーマ通りに200が返って、アクセストークンを返すこと' do
+        it '200が返って、アクセストークンを返すこと' do
           post '/api/token', headers:, params: { email: user.email, password: user.password }, as: :json
           assert_response_schema_confirm(200)
         end
@@ -20,7 +20,7 @@ RSpec.describe 'ApiTokens' do
       context 'ユーザがAdminでない場合' do
         let(:noadmin) { create(:user, :noadmin) }
 
-        it 'スキーマ通りに403が返って、エラーメッセージを返すこと' do
+        it '403が返って、エラーメッセージを返すこと' do
           post '/api/token', headers:, params: { email: noadmin.email, password: noadmin.password }, as: :json
           assert_response_schema_confirm(403)
         end
@@ -28,7 +28,7 @@ RSpec.describe 'ApiTokens' do
     end
 
     context 'ユーザが存在しない場合' do
-      it 'スキーマ通りに401が返って、エラーメッセージを返すこと' do
+      it '401が返って、エラーメッセージを返すこと' do
         post '/api/token', headers:, params: { email: 'test@hogehgoe.com', password: 'invalid password' }, as: :json
         assert_response_schema_confirm(401)
       end
